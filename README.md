62adcb9a24b633ed659854fde0ecb1dde5dde5d8
Nikola Lakic <nikolal@disroot.org>
2020-08-24 16:17:59 +0200

    Changed repo to Github

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # changedip
-Send PGP encrypted email when IP address of device running script changes
+Python script for automated response when public IP address changes of device running it, with encrypted email using gnupg
+
+### NOTE: email and password are stored plaintext in two separate files: email.txt and password.txt, use it with servers you trust.
+
+#### Dependencies:
+
+##### Python2: if you are running raspberry pi with Raspbian you need python2 and requireing modules, you install them with these commands:
+
+`sudo apt install python-pip` 
+
+`sudo pip install requests python-gnupg` 
+
+##### Python3:
+
+`sudo apt install python3-pip`
+
+`sudo pip3 install python-gnupg`
+
+#### Usage:
+
+You begin with entering your credentials for email account you want to use to send email, and you must have public key in your gnupg keyring for encryption of mail to recipient you want to send to.
+
+Just edit line  __13__ and lines __49__ and __50__, you will see explanation for them in code.
+
+Script recognises some servers with smtp protocol and will automatically set correct one (if its listed) when sending email.
+
+Usable with cron.
+
+Any suggestions appreciated.
+
---
+++ changedip.py
@@ -0,0 +1,70 @@
+#! /usr/bin/python3
+
+import requests
+import smtplib
+import gnupg
+import os
+import getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+ip = requests.get('https://api.ipify.org').text
+ip = str(ip)
+#You edit next line
+old_ip = 'Enter your current IP here'
+userpath = os.getcwd()
+user = getpass.getuser()
+gpg = gnupg.GPG(gnupghome=('/home/{}/.gnupg').format(user))
+servers = ['disroot.org', 'gmail.com', 'posteo.net', 'yahoo.com', 'live.com']
+cockliservers = ['cock.li','airmail.cc','420blaze.it','aaathats3as.com','cumallover.me','goat.si','horsefucker.org','national.shitposting','nigge.rs','tfwno.gf','cock.lu','cock.email','firemail.cc','hitler.rocks','getbackinthe.kitchen','memeware.net','cocaine.ninja','waifu.club','rape.lol','nuke.africa']
+
+password = open('password.txt').read()
+password = password.partition('\n')[0]
+username = open('email.txt').read()
+username = username.partition('\n')[0]
+providername = username.split('@',1)[1]
+
+if any(providername in i for i in servers):
+    if providername == 'disroot.org':
+        server = providername
+    elif providername == 'gmail.com':
+        server = 'smtp.gmail.com'
+    elif providername == posteo.net:
+        server = 'posteo.de'
+    elif providername == 'startmail.com':
+        server = 'smtp.startmail.com'
+    elif providername == 'yahoo.com':
+        server = 'smtp.mail.yahoo.com'
+    elif providername == 'live.com':
+        server = 'smtp.live.com'
+    else:
+        print('Provider not in the list of providers, contact maintainer of this project for adding provider you use to list.')
+if any(providername in i for i in cockliservers):
+    server = 'mail.cock.li'
+
+if old_ip != ip:
+    msg = MIMEMultipart()
+    msg['From'] = username
+    # You edit next two lines only
+    RecipientID = 'nikolal@disroot.org' 
+    Recipient = 'nikola@lakic.one'
+    msg['To'] = Recipient
+    msg['Subject'] = 'Raspberry Pi IP changed!'
+    mailbody = ('New IP of raspberry is: {}').format(ip)
+    secretmessage = (str(gpg.encrypt(mailbody, RecipientID, always_trust=True)))
+    msg.attach(MIMEText(secretmessage, 'plain'))
+    smtpObj = smtplib.SMTP(server, 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.ehlo()
+    smtpObj.login(username, password)
+    try:
+        smtpObj.sendmail(username, Recipient, msg.as_string())
+        smtpObj.close()
+    except Exception as e:
+        print(e)
+else:
+    pass
+exit()
